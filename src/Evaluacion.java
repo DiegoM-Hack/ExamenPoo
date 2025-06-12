95ebbb4a5d7e2fbc4819c2c2399ff0bd479d7d75
DiegoM-Hack <santitodiego12@gmail.com>
2025-06-12 10:26:33 -0500

    final

--- src/Evaluacion.java
+++ src/Evaluacion.java
@@ -7,7 +7,7 @@ public class Evaluacion {
         if (nota >= 0 && nota <= 10) {
             this.nota = nota;
         } else {
-            throw new IllegalArgumentException("La nota debe estar entre 0 y 10.");
+            System.out.println("La nota debe estar entre 0 y 10.");
         }
     }
 
@@ -16,7 +16,6 @@ public class Evaluacion {
     }
 
     public void certificar(double nota, int tipoCurso ){
-        System.out.println("Cual fue tu curso: 1) Regular 2)Certificado" );
         if (nota >= 7 && tipoCurso == 2) {
             System.out.println("Aprueba el curso y obtienes una certificacion");
         }else if (nota >= 7 && tipoCurso == 1){
